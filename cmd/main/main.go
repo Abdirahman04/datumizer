8e8a8c43ca67bf281c432a701c5206356ecea480
Abdirahman04 <abdixcom@gmail.com>
2024-05-07 20:32:00 +0300

    updated main function

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/Abdirahman04/datumizer/boolka"
+	"github.com/Abdirahman04/datumizer/dateka"
 	"github.com/Abdirahman04/datumizer/floatka"
 	"github.com/Abdirahman04/datumizer/intka"
 	"github.com/Abdirahman04/datumizer/stringka"
@@ -15,4 +16,5 @@ func main() {
 	fmt.Println("RandomInt => ", intka.RandomInt(4))
 	fmt.Println("RandomFloat => ", floatka.RandomFloat(3, 2))
 	fmt.Println("RandomBool => ", boolka.RandomBool())
+	fmt.Println("RandomDate => ", dateka.RandomDate())
 }
