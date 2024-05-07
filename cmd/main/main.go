bbb92915024836b4d087a02139ae2147ea30f92f
Abdirahman04 <abdixcom@gmail.com>
2024-05-07 15:14:56 +0300

    updated main function

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/Abdirahman04/datumizer/stringka"
 )
 
 func main() {
 	fmt.Println("Hello")
+
+	fmt.Println("RandomString => ", stringka.RandomString(6))
 }
